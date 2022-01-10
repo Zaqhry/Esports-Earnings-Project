4ed6f0d95c59240869b0ac6408c77ff78ca61ce0
Zaqhry <96840072+Zaqhry@users.noreply.github.com>
2022-01-10 18:34:59 -0500

    Update Esports Earnings SQL.sql

--- Esports Earnings SQL.sql
+++ Esports Earnings SQL.sql
@@ -24,54 +24,64 @@ FROM ESL
 
 --# of players per each game 
 
-SELECT Game,COUNT(Gamertag) TotalPlayers
+SELECT Game,
+       COUNT(Gamertag) TotalPlayers
 FROM ESL
-GROUP BY Game 
+  GROUP BY Game 
 
 
 
 --# of players per country 
 
-SELECT Country,COUNT(Gamertag) TotalPlayers
+SELECT Country,
+       COUNT(Gamertag) TotalPlayers
 FROM ESL
-GROUP BY Country
-ORDER BY TotalPlayers DESC
+  GROUP BY Country
+  ORDER BY TotalPlayers DESC
 
 
 
 --Total prize per country 
 
-SELECT Country,ROUND(SUM(TotalUSDPrize),2) Prize
+SELECT Country,
+       ROUND(SUM(TotalUSDPrize),2) Prize
 FROM ESL 
-GROUP BY Country
-ORDER BY Prize DESC
+  GROUP BY Country
+  ORDER BY Prize DESC
 
 
 
 --Average earnings per country
 
-SELECT Country,ROUND(AVG(TotalUSDPrize),2) Prize
+SELECT Country,
+       ROUND(AVG(TotalUSDPrize),2) Prize
 FROM ESL 
-GROUP BY Country
-ORDER BY Prize DESC
+  GROUP BY Country
+  ORDER BY Prize DESC
 
 
 
 --Highest earnings per country 
 
-SELECT Country,ROUND(MAX(TotalUSDPrize),2) Prize
+SELECT Country,
+       ROUND(MAX(TotalUSDPrize),2) Prize
 FROM ESL 
-GROUP BY Country
-ORDER BY Prize DESC
+  GROUP BY Country
+  ORDER BY Prize DESC
 
 
 
 --Player with the highest earnings 
 
-SELECT FirstName,LastName,Gamertag,ROUND(SUM(TotalUSDPrize),2) Prize
+SELECT FirstName,
+       LastName,
+       Gamertag,
+       ROUND(SUM(TotalUSDPrize),2) Prize
 FROM ESL 
-GROUP BY FirstName,LastName,Gamertag
-ORDER BY Prize DESC
+  GROUP BY FirstName,
+           LastName,
+           Gamertag
+  ORDER BY Prize DESC
 
 
 
@@ -84,146 +94,203 @@ FROM ESL
 
 --Average earnings per player across a distinct game 
 
-SELECT Game,ROUND(AVG(TotalUSDPrize),2) AvgEarnings 
+SELECT Game,
+       ROUND(AVG(TotalUSDPrize),2) AvgEarnings 
 FROM ESL 
-GROUP BY Game 
-ORDER BY AvgEarnings DESC
+  GROUP BY Game 
+  ORDER BY AvgEarnings DESC
 
 
 
 --Average earnings per game 
 
-SELECT Game,ROUND(AVG(TotalUSDPrize),2) Prize
+SELECT Game,
+       ROUND(AVG(TotalUSDPrize),2) Prize
 FROM ESL 
-GROUP BY Game 
-ORDER BY Prize DESC
+  GROUP BY Game 
+  ORDER BY Prize DESC
 
 
 
 --Total earnings per game 
 
-SELECT Game,ROUND(SUM(TotalUSDPrize),2) Prize
+SELECT Game,
+       ROUND(SUM(TotalUSDPrize),2) Prize
 FROM ESL 
-GROUP BY Game 
-ORDER BY Prize DESC
+  GROUP BY Game 
+  ORDER BY Prize DESC
 
 
 
 --Average earnings per genre 
 
 
-SELECT Genre,ROUND(AVG(TotalUSDPrize),2) AvgEarnings 
+SELECT Genre,
+       ROUND(AVG(TotalUSDPrize),2) AvgEarnings 
 FROM ESL 
-GROUP BY Genre
-ORDER BY AvgEarnings DESC
+  GROUP BY Genre
+  ORDER BY AvgEarnings DESC
 
 
 
 --Total earnings per genre 
 
 
-SELECT Genre,ROUND(SUM(TotalUSDPrize),2) TotalEarnings 
+SELECT Genre,
+       ROUND(SUM(TotalUSDPrize),2) TotalEarnings 
 FROM ESL 
-GROUP BY Genre
-ORDER BY TotalEarnings DESC
+  GROUP BY Genre
+  ORDER BY TotalEarnings DESC
 
 
 
 --Top 5 Highest Earners in Dota 2 
 
-SELECT TOP 5 FirstName,LastName,Gamertag,SUM(TotalUSDPrize) TotalEarnings
+SELECT TOP 5 FirstName,
+             LastName,
+             Gamertag,
+             SUM(TotalUSDPrize) TotalEarnings
 FROM ESL 
 WHERE Game = 'Dota 2'
-GROUP BY FirstName,LastName,Gamertag 
-ORDER BY TotalEarnings DESC
+  GROUP BY FirstName,
+           LastName,
+           Gamertag 
+  ORDER BY TotalEarnings DESC
 
 
 
 --Top 5 Highest Earners in Counter-Strike: Global Offensive 
 
-SELECT TOP 5 FirstName,LastName,Gamertag,SUM(TotalUSDPrize) TotalEarnings
+SELECT TOP 5 FirstName,
+             LastName,
+             Gamertag,
+             SUM(TotalUSDPrize) TotalEarnings
 FROM ESL 
 WHERE Game = 'Counter-Strike: Global Offensive'
-GROUP BY FirstName,LastName,Gamertag 
-ORDER BY TotalEarnings DESC
+  GROUP BY FirstName,
+           LastName,
+           Gamertag 
+  ORDER BY TotalEarnings DESC
 
 
 
 --Top 5 Highest Earners in Fortnite 
 
-SELECT TOP 5 FirstName,LastName,Gamertag,SUM(TotalUSDPrize) TotalEarnings
+SELECT TOP 5 FirstName,
+             LastName,
+             Gamertag,
+             SUM(TotalUSDPrize) TotalEarnings
 FROM ESL 
 WHERE Game = 'Fortnite'
-GROUP BY FirstName,LastName,Gamertag 
-ORDER BY TotalEarnings DESC
+  GROUP BY FirstName,
+           LastName,
+           Gamertag 
+  ORDER BY TotalEarnings DESC
 
 
 
 --Top 5 Highest Earners in League of Legends 
 
-SELECT TOP 5 FirstName,LastName,Gamertag,SUM(TotalUSDPrize) TotalEarnings
+SELECT TOP 5 FirstName,
+             LastName,
+             Gamertag,
+             SUM(TotalUSDPrize) TotalEarnings
 FROM ESL 
 WHERE Game = 'League of Legends'
-GROUP BY FirstName,LastName,Gamertag 
-ORDER BY TotalEarnings DESC
+  GROUP BY FirstName,
+           LastName,
+           Gamertag 
+  ORDER BY TotalEarnings DESC
 
 
 
 --Top 5 Highest Earners in Starcraft II 
 
-SELECT TOP 5 FirstName,LastName,Gamertag,SUM(TotalUSDPrize) TotalEarnings
+SELECT TOP 5 FirstName,
+             LastName,
+             Gamertag,
+             SUM(TotalUSDPrize) TotalEarnings
 FROM ESL 
 WHERE Game = 'Starcraft II'
-GROUP BY FirstName,LastName,Gamertag 
-ORDER BY TotalEarnings DESC
+  GROUP BY FirstName,
+           LastName,
+           Gamertag 
+  ORDER BY TotalEarnings DESC
 
 
 
 --Top 5 Highest Earners in Hearthstone 
 
-SELECT TOP 5 FirstName,LastName,Gamertag,SUM(TotalUSDPrize) TotalEarnings
+SELECT TOP 5 FirstName,
+             LastName,
+             Gamertag,
+             SUM(TotalUSDPrize) TotalEarnings
 FROM ESL 
 WHERE Game = 'Hearthstone'
-GROUP BY FirstName,LastName,Gamertag 
-ORDER BY TotalEarnings DESC
+  GROUP BY FirstName,
+           LastName,
+           Gamertag 
+  ORDER BY TotalEarnings DESC
 
 
 
 --Top 5 Highest Earners in PUBG 
 
-SELECT TOP 5 FirstName,LastName,Gamertag,SUM(TotalUSDPrize) TotalEarnings
+SELECT TOP 5 FirstName,
+             LastName,
+             Gamertag,
+             SUM(TotalUSDPrize) TotalEarnings
 FROM ESL 
 WHERE Game = 'PUBG'
-GROUP BY FirstName,LastName,Gamertag 
-ORDER BY TotalEarnings DESC
+  GROUP BY FirstName,
+           LastName,
+           Gamertag 
+  ORDER BY TotalEarnings DESC
 
 
 
 --Top 5 Highest Earners in Overwatch 
 
-SELECT TOP 5 FirstName,LastName,Gamertag,SUM(TotalUSDPrize) TotalEarnings
+SELECT TOP 5 FirstName,
+             LastName,
+             Gamertag,
+             SUM(TotalUSDPrize) TotalEarnings
 FROM ESL 
 WHERE Game = 'Overwatch'
-GROUP BY FirstName,LastName,Gamertag 
-ORDER BY TotalEarnings DESC
+  GROUP BY FirstName,
+           LastName,
+           Gamertag 
+  ORDER BY TotalEarnings DESC
 
 
 
 --Top 5 Highest Earners in Heroes of the Storm 
 
 
-SELECT TOP 5 FirstName,LastName,Gamertag,SUM(TotalUSDPrize) TotalEarnings
+SELECT TOP 5 FirstName,
+             LastName,
+             Gamertag,
+             SUM(TotalUSDPrize) TotalEarnings
 FROM ESL 
 WHERE Game = 'Heroes of the Storm'
-GROUP BY FirstName,LastName,Gamertag 
-ORDER BY TotalEarnings DESC
+  GROUP BY FirstName,
+           LastName,
+           Gamertag 
+  ORDER BY TotalEarnings DESC
 
 
 
 --Top 5 Highest Earners in Arena of Valor 
 
-SELECT TOP 5 FirstName,LastName,Gamertag,SUM(TotalUSDPrize) TotalEarnings
+SELECT TOP 5 FirstName,
+             LastName,
+             Gamertag,
+             SUM(TotalUSDPrize) TotalEarnings
 FROM ESL 
 WHERE Game = 'Arena of Valor'
-GROUP BY FirstName,LastName,Gamertag 
+GROUP BY FirstName,
+         LastName,
+         Gamertag 
 ORDER BY TotalEarnings DESC
+
+
